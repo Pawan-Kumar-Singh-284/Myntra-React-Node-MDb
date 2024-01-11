d3dd3543706c64aeb30d00fa476cab589effd1bc
Pawan-Kumar-Singh-284 <19krpawan@gmail.com>
2024-01-11 20:38:23 +0530

    edited

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../index.css";
-import FetchItems from "../components/fetchItems";
+import FetchItems from "../components/FetchItems";
 function App() {
   return (
     <>
